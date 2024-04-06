bebe5e61296d2267ad1e0ff7b8d0e36bbd524faf
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-04-06 10:27:21 +0300

    [ADD] Login and Register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import QuestionPage from "./components/QuestionPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => (
         <Router>
             <Navbar />
             <div className="mt-20">
                 <Routes>
-                    <Route path='/homePage' element={<HomePage />} />
                     <Route path='/' element={<HomePage />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/homePage' element={<HomePage />} />
                     <Route path='/questions/:questionId' element={<QuestionPage />} />
+
+                    <Route path='*' element={<h1>Not Found</h1>} />
                 </Routes>
             </div>
         </Router>

2c2808539f8c9555a144c02c373d06cc284ae2bf
Ivan Georgiev <71922026+IvanGeorgievProgramming@users.noreply.github.com>
2024-04-24 17:53:30 +0300

    Update services.py

--- QuestionService/questions/services.py
+++ QuestionService/questions/services.py
@@ -1,22 +1,56 @@
 from questions.models import Question
 from extensions import db
 
-def create_question(data):
+def create_question_object(data):
     new_question = Question(
         title=data['title'],
         body=data['body'],
-        user_id=data['user_id']
+        author_id=data['author_id']
     )
     
     db.session.add(new_question)
     db.session.commit()
 
-    return new_question, None
+    new_question = Question.query.order_by(Question.id.desc()).first()
+
+    message = {
+        "id": new_question.id,
+        "title": new_question.title,
+        "body": new_question.body,
+        "is_resolved": new_question.is_resolved,
+        "author_id": new_question.author_id
+    }
+
+    return message
 
 def get_question_by_id(question_id):
     question = Question.query.get(question_id)
 
-    return question, None
+    message = {
+        "id": question.id,
+        "title": question.title,
+        "body": question.body,
+        "is_resolved": question.is_resolved,
+        "author_id": question.author_id
+    }
+
+    return message
+
+def get_all_questions():
+    questions = Question.query.all()
+
+    message = {}
+
+    for question in questions:
+        message[question.id] = {
+            "id": question.id,
+            "title": question.title,
+            "body": question.body,
+            "is_resolved": question.is_resolved,
+            "author_id": question.author_id
+        }
+
+    return message
 
 def resolve_question_by_id(question_id):
     question = Question.query.get(question_id)
@@ -24,4 +58,21 @@ def resolve_question_by_id(question_id):
 
     db.session.commit()
 
-    return question, None
+    message = {
+        "id": question.id,
+        "title": question.title,
+        "body": question.body,
+        "is_resolved": question.is_resolved,
+        "author_id": question.author_id
+    }
+
+    return message
+
+def is_resolved_by_id(question_id):
+    question = Question.query.get(question_id)
+
+    message = {
+        "is_resolved": question.is_resolved
+    }
+
+    return message

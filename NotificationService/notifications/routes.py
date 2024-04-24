894165f0dce403fc641640e65370e94eabe8824e
Desislava Stefanova <71987375+Dessyst@users.noreply.github.com>
2024-04-24 22:51:06 +0300

    Add files via upload

--- NotificationService/notifications/routes.py
+++ NotificationService/notifications/routes.py
@@ -9,13 +9,17 @@ def send_notification():
     data = request.json
     recipient_email = data.get('recipient_email')
     body = data.get('body')
+    subject = data.get('subject')
 
     if not recipient_email or not body:
         return jsonify({'message': 'Recipient email and body are required'}), 400
 
-    success, error_message = send_email_notification(recipient_email, body)
+    success, error_message = send_email_notification(recipient_email, subject, body)
 
     if success:
         return jsonify({'message': 'Notification sent successfully'}), 200
     else:
         return jsonify({'message': f'Failed to send notification: {error_message}'}), 500
+        
+    
+    
--- NotificationService/notifications/services.py
+++ NotificationService/notifications/services.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 EMAIL_SENDER = 'duohealth0@gmail.com'
 EMAIL_PASSWORD = 'gxopinvriohdwyzd'
 
-def send_email_notification(user_email, body):
+def send_email_notification(user_email, subject, body):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:           # Create a secure SSL context
         smtp.ehlo()                                             # Identify yourself to an ESMTP server using EHLO
         smtp.starttls()                                         # Put the SMTP connection in TLS (Transport Layer Security) mode
@@ -13,7 +13,7 @@ def send_email_notification(user_email, body):
 
         smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)                # Log in on an SMTP server that requires authentication
 
-        subject = 'MiniStackOverflow response'                # Define the subject and body of the email
+        #subject = 'MiniStackOverflow response'                # Define the subject and body of the email
 
         msg = f'Subject: {subject}\n\n{body}'                   # Format the message   
 

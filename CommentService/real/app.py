37b029be4c3f45ef2d08d484c33b79ab01234328
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-04-25 05:26:33 +0300

    [ADD] CommentService kubernetes deployment ready

--- CommentService/real/app.py
+++ CommentService/real/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from questions.routes import questions_bp
+from comments.routes import comments_bp
 from extensions import initialize_extensions, db
 from config import Config
 
 def create_app():
     api = Flask(__name__)
     api.config.from_object(Config)
-    api.register_blueprint(questions_bp)
+    api.register_blueprint(comments_bp)
     initialize_extensions(api)
 
     with api.app_context():

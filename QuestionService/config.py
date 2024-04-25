78695c84cfd9172ac4714af46fb7115846bd829d
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-04-25 05:18:50 +0300

    [UPDATE] QuestionService ready for kubernetes deployment

--- QuestionService/config.py
+++ QuestionService/config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    db_host = os.environ.get('DB_HOST')
-    db_user = os.environ.get('DB_USER')
-    db_password = os.environ.get('DB_PASSWORD')
-    db_name = os.environ.get('DB_NAME')
+    # db_host = os.environ.get('DB_HOST')
+    # db_user = os.environ.get('DB_USER')
+    # db_password = os.environ.get('DB_PASSWORD')
+    # db_name = os.environ.get('DB_NAME')
 
-    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:password@mysql/microservice-questions'

6810156db59381df5bc570c7a823570f9c2defee
Hrisitqn Petkov <hristiyanpetkov05@gmail.com>
2024-04-25 06:52:10 +0300

    [UPDATE] Kubernetes deployment ready

--- CommentService/real/config.py
+++ CommentService/real/config.py
@@ -1,17 +1,12 @@
 import os
 
 class Config:
-    db_host = os.getenv('DB_HOST')
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_name = os.getenv('DB_NAME')
+    # db_host = os.getenv('DB_HOST')
+    # db_user = os.getenv('DB_USER')
+    # db_password = os.getenv('DB_PASSWORD')
+    # db_name = os.getenv('DB_NAME')
 
-    print(f"DB_HOST: {db_host}")
-    print(f"DB_USER: {db_user}")
-    print(f"DB_PASSWORD: {db_password}")
-    print(f"DB_NAME: {db_name}")
-
-    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:password@mysql/microservice_comments'
 
 if __name__ == '__main__':
     config = Config()

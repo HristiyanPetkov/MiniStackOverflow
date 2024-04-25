a277c01329ce4d468d32a5b97b442f13ff67fe7f
Aleksandar <ai5517085@gmail.com>
2024-04-25 03:23:31 +0300

    meh

--- CommentService/try/main.py
+++ CommentService/try/main.py
@@ -23,6 +23,20 @@ def getDBSession():
     return session
 
 
+# def getDBSession2():
+#     # Create and get a Cassandra session
+#     cloud_config= {
+#             #'secure_connect_bundle': os.environ.get('ASTRA_PATH_TO_SECURE_BUNDLE')
+#             'secure_connect_bundle': '/Users/sasukoboy/Documents/GitHub/MiniStackOverflow/CommentService/secure-connect-ministackdb.zip'
+#             #'use_default_tempdir' : True
+#     }
+#     auth_provider = PlainTextAuthProvider(os.environ.get('ASTRA_CLIENT_ID'), os.environ.get('ASTRA_CLIENT_SECRET'))
+#     print(auth_provider)
+#     cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+#     session = cluster.connect()
+#     return session
+
+
 def generateInsertData(answer_id, user_id, body, session):
     """A function which completes generation of data and insertion of data to Cassandra"""
     insert_query = session.prepare("""

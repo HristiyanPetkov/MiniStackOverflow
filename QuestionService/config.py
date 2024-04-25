d770f80925868615ba5c557046b5f512323dda42
Ivan Georgiev Programming <IvanGeorgievProgramming@gmail.com>
2024-04-25 19:30:06 +0300

    Question Service Modifications

--- QuestionService/config.py
+++ QuestionService/config.py
@@ -1,9 +1,20 @@
 import os
+from dotenv import load_dotenv
 
 class Config:
-    db_host = os.environ.get('DB_HOST')
-    db_user = os.environ.get('DB_USER')
-    db_password = os.environ.get('DB_PASSWORD')
-    db_name = os.environ.get('DB_NAME')
+    load_dotenv()
+
+    db_host = os.getenv('DB_HOST')
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_name = os.getenv('DB_NAME')
+
+    print(f"DB_HOST: {db_host}")
+    print(f"DB_USER: {db_user}")
+    print(f"DB_PASSWORD: {db_password}")
+    print(f"DB_NAME: {db_name}")
 
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}'
+
+if __name__ == '__main__':
+    config = Config()
